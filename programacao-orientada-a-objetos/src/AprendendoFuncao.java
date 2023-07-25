a97e9789bec83713a84d9f75898f1a8e7433f351
alexaniatoma <alexaniatoma@hotmail.com>
2023-07-25 18:47:33 -0300

    classes produto e data

--- programacao-orientada-a-objetos/src/AprendendoFuncao.java
+++ programacao-orientada-a-objetos/src/AprendendoFuncao.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AprendendoFuncao {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("Vamos ver qual é o maior número?");
 		System.out.println("Digite um primeiro número");
 		int num1 = scan.nextInt();
@@ -13,36 +13,36 @@ public class AprendendoFuncao {
 		int num2 = scan.nextInt();
 		System.out.println("Digite um terceiro número");
 		int num3 = scan.nextInt();
-		
-		//if(num1 > num2 && num1 > num3) {
-			//System.out.println(num1 + " é o maior número digitado");
-		//}else if (num2 > num3 && num2 > num1) {
-			//System.out.println(num2 + " é o maior numero digitado");
-		//}else {
-		//	System.out.println(num3 +" é o maior numero digitado");
-		//}
-	
+
+		// if(num1 > num2 && num1 > num3) {
+		// System.out.println(num1 + " é o maior número digitado");
+		// }else if (num2 > num3 && num2 > num1) {
+		// System.out.println(num2 + " é o maior numero digitado");
+		// }else {
+		// System.out.println(num3 +" é o maior numero digitado");
+		// }
+
 		int numeroMaior = max(num1, num2, num3);
-		
+
 		resultado(numeroMaior);
-	
+
 		scan.close();
-	
+
 	}
-	
+
 	public static int max(int x, int y, int z) {
 		int aux;
-		if(x > y && x > z) {
+		if (x > y && x > z) {
 			aux = x;
-		}else if( y > z) {
+		} else if (y > z) {
 			aux = y;
-		}else {
+		} else {
 			aux = z;
-			
+
 		}
 		return aux;
 	}
-	
+
 	public static void resultado(int value) {
 		System.out.println("Numero maior é: " + value);
 	}
---
+++ programacao-orientada-a-objetos/src/classe/DataTeste.java
@@ -0,0 +1,26 @@
+package classe;
+
+public class DataTeste {
+	public static void main(String[] args) {
+		
+		Data niverAle = new Data();
+		niverAle.dia = 30;
+		niverAle.mes = 12;
+		niverAle.ano = 1984;
+		
+		System.out.printf("Data aniversário Alexania %d %d %d\n" , niverAle.dia, niverAle.mes, niverAle.ano);
+		
+		
+		Data niverCelso = new Data();
+		niverCelso.dia = 02;
+		niverCelso.mes = 8;
+		niverCelso.ano = 1965;
+		
+		System.out.printf("Data aniversário Celso %d %d %d" , niverCelso.dia, niverCelso.mes, niverCelso.ano);
+		
+		
+	}
+	
+	
+
+}
