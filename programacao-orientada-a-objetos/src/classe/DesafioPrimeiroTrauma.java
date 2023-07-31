943afec4af0cf18ce21271807e6f25aa8f9b8fd7
alexaniatoma <alexaniatoma@hotmail.com>
2023-07-31 15:23:28 -0300

    desafio primeiro trauma

---
+++ programacao-orientada-a-objetos/src/classe/DesafioPrimeiroTrauma.java
@@ -0,0 +1,13 @@
+package classe;
+
+public class DesafioPrimeiroTrauma {
+	
+	static int a = 3; // não pode mexer nesta linha
+	
+	public static void main(String[] args) {
+	
+		
+		System.out.println(a);
+	}
+
+}
